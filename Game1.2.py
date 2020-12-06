c6cca2a02e1d44d74fb166f751153ff2650b64f3
Patrick <puhstp@googlemail.com>
2020-12-06 18:14:58 +0100

    Steuerung implementiert

--- Game1.2.py
+++ Game1.2.py
@@ -1,6 +1,6 @@
 import pygame   #importiert die Pygamme Bibliothek in das Skript   
 import os   
-from pygame.constants import (QUIT, KEYDOWN, KEYUP, K_ESCAPE, K_LEFT, K_RIGHT)  #importiert bestimmte Konstanten aus dem OS Modul in das Skript
+from pygame.constants import (QUIT, KEYDOWN, KEYUP, K_ESCAPE, K_LEFT, K_RIGHT, K_UP, K_DOWN)  #importiert bestimmte Konstanten aus dem OS Modul in das Skript
 
 class Settings(object):
     def __init__(self):
@@ -13,6 +13,35 @@ class Settings(object):
 
     def get_dim(self):
         return (self.width, self.height)    #gibt die Breite x Höhe zurück
+
+
+
+class Dog(pygame.sprite.Sprite):    #Objekt Dog
+    def __init__(self, settings):
+        pygame.sprite.Sprite.__init__(self) #Da von den Pygame-Sprites geerbt wird, werden hier die Funktion initalisiert
+        self.settings = settings
+        self.pygame = pygame
+        self.image = pygame.image.load(os.path.join(self.settings.images_path, "doggo.png")).convert_alpha() #das bmp für den Hund wird reinimportiert
+        self.image = pygame.transform.scale(self.image, (110, 60))   #Größe des Bildes
+        self.rect = self.image.get_rect()
+        self.rect.left = (settings.width - self.rect.width) // 2    #Diese und die nächste Zeile sind dafür zuständig, dass das bmp mittig zentriert ist
+        self.rect.top = settings.height - self.rect.height - 10
+        self.direction = 0
+        self.direction2 = 0
+        self.speed = 5
+
+    def update(self):
+        newleft = self.rect.left + (self.direction * self.speed)
+        newright = newleft + self.rect.width
+        newtop = self.rect.top + (self.direction2 * self.speed)
+        newbottom = newtop + self.rect.height
+        
+        if newleft > 0 and newright < settings.width:
+            self.rect.left = newleft
+        if newtop > 0 and newbottom < settings.height:
+            self.rect.top = newtop
+        
+        
 
 class Game(object):
     def __init__(self, pygame, settings):
@@ -38,6 +67,25 @@ class Game(object):
                 elif event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         self.done = True
+                    
+                    elif event.key == K_LEFT:
+                        self.dog.direction = -1
+                    elif event.key == K_RIGHT:
+                        self.dog.direction = 1
+                   
+                    elif event.key == K_UP:
+                        self.dog.direction2 = -1
+                    elif event.key == K_DOWN:
+                        self.dog.direction2 = 1
+
+                elif event.type == KEYUP:
+                    if event.key == K_LEFT or event.key == K_RIGHT:
+                        self.dog.direction = 0
+                    elif event.key == K_UP or event.key == K_DOWN:
+                        self.dog.direction2 = 0
+
+
+            self.update()
             self.draw()
 
     def draw(self): #alle Objekte werden gezeichnet und der Bildschirm wird aktualisiert
@@ -45,16 +93,8 @@ class Game(object):
         self.all_doggos.draw(self.screen)
         self.pygame.display.flip()
 
-class Dog(pygame.sprite.Sprite):    #Objekt Dog
-    def __init__(self, settings):
-        pygame.sprite.Sprite.__init__(self) #Da von den Pygame-Sprites geerbt wird, werden hier die Funktion initalisiert
-        self.settings = settings
-        self.pygame = pygame
-        self.image = pygame.image.load(os.path.join(self.settings.images_path, "doggo.png")).convert_alpha() #das bmp für den Hund wird reinimportiert
-        self.image = pygame.transform.scale(self.image, (110, 60))   #Größe des Bildes
-        self.rect = self.image.get_rect()
-        self.rect.left = (settings.width - self.rect.width) // 2    #Diese und die nächste Zeile sind dafür zuständig, dass das bmp mittig zentriert ist
-        self.rect.top = settings.height - self.rect.height - 10
+    def update(self):
+        self.all_doggos.update()
 
 if __name__ == '__main__':  #main-Schleife   
                                     
